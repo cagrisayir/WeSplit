3b181ee494298baa21881da88cde748a5ce24f83
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-02 10:09:06 +0300

    Took amount from user

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages: [Int] = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
         }
-        .padding()
+        
+        
     }
 }
 
